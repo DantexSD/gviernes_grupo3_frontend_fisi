7af16ac82dd6cc1619633b21ee221ba4504ece1a
AngeloIngaJP <angelo.inga@unmsm.edu.pe>
2022-01-19 21:29:29 -0500

    19/01/22

--- Navigation.js
+++ Navigation.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import { NavigationContainer } from "@react-navigation/native"
-import { createStackNavigator } from "@react-navigation/stack"
-import CalificacionDocente from './screens/CalificacionDocente'
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import CalificacionDocente from "./screens/CalificacionDocente";
+import RankingDocentes from "./screens/RankingDocentes";
 
 export default function Navigation() {
+  const Stack = createStackNavigator();
 
-    const Stack = createStackNavigator()
+  //Header Show
+  const screenOptions = {
+    headerShown: false,
+  };
 
-    //Header Show
-    const screenOptions = {
-        headerShown: false
-    }
-
-    return (
-        <NavigationContainer>
-            {/* initialRouteName -> tu pantalla de inicio */}
-            <Stack.Navigator initialRouteName="CalificacionDocente" screenOptions={screenOptions} >
-                {/* Creas tus pantallas */}
-                <Stack.Screen name="CalificacionDocente" component={CalificacionDocente} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  return (
+    <NavigationContainer>
+      {/* initialRouteName -> tu pantalla de inicio */}
+      <Stack.Navigator
+        initialRouteName="RankingDocentes"
+        screenOptions={screenOptions}
+      >
+        {/* Creas tus pantallas */}
+        <Stack.Screen
+          name="CalificacionDocente"
+          component={CalificacionDocente}
+        />
+        <Stack.Screen name="RankingDocentes" component={RankingDocentes} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/CalificacionDocente.js
+++ screens/CalificacionDocente.js
@@ -1,31 +1,29 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from 'react-native';
-import React from 'react';
-import Summary from '../components/calificacion/Summary';
-import Comments from '../components/calificacion/Comments';
+import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
+import React from "react";
+import Summary from "../components/calificacion/Summary";
+import Comments from "../components/calificacion/Comments";
+import RankingDocente from "./RankingDocentes";
 
 export default function CalificacionDocente() {
-
-    const data = [
-        {
-            
-        }
-    ]
+  const data = [{}];
 
   return (
     <View style={styles.container}>
       <Summary />
       <View style={styles.containerComment}>
-        <Text style={styles.numberComments}>2 calificaciones de estudiantes</Text>
+        <Text style={styles.numberComments}>
+          2 calificaciones de estudiantes
+        </Text>
         <View style={styles.header}>
-            <Text>Calficación</Text>
-            <Text>Curso</Text>
-            <Text>Comentarios</Text>
-        </View>    
+          <Text>Calficación</Text>
+          <Text>Curso</Text>
+          <Text>Comentarios</Text>
+        </View>
         <ScrollView showsVerticalScrollIndicator={false}>
-            <Comments />
+          <Comments />
         </ScrollView>
-        <Pressable titleSize={20} style={styles.button} >
-            <Text style={styles.buttonText}>Calificar docente</Text>
+        <Pressable titleSize={20} style={styles.button}>
+          <Text style={styles.buttonText}>Calificar docente</Text>
         </Pressable>
       </View>
     </View>
@@ -33,38 +31,38 @@ export default function CalificacionDocente() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#fff",
-        flex: 1
-    },
-    containerComment: {
-        margin: 20
-    },
-    numberComments: {
-        fontSize: 18,
-        fontWeight: "bold",
-        marginBottom: 10
-    },
-    header: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        // margin: 20,
-        padding: 10,
-        marginBottom: 20,
-        borderRadius: 5,
-        backgroundColor: "#c4c4c4",
-    }, 
-    button: {
-        backgroundColor: "#0096F6",
-        alignItems: "center",
-        justifyContent: "center",
-        minHeight: 42,
-        borderRadius: 4,
-        marginTop: 20
-    },
-    buttonText: {
-        fontWeight: "600",
-        color: "#fff",
-        fontSize: 20
-    },
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+  },
+  containerComment: {
+    margin: 20,
+  },
+  numberComments: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // margin: 20,
+    padding: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+    backgroundColor: "#c4c4c4",
+  },
+  button: {
+    backgroundColor: "#0096F6",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: 42,
+    borderRadius: 4,
+    marginTop: 20,
+  },
+  buttonText: {
+    fontWeight: "600",
+    color: "#fff",
+    fontSize: 20,
+  },
 });
