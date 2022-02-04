dccd0c12849e3d1363169bd55026bc7c9a69a8f6
sebastian ccv <sebastian.ccopacondori@unmsm.edu.pe>
2022-02-03 19:01:30 -0500

    merge final v2

--- axios.js
+++ axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 //API URL backend
 const instance = axios.create({
-    baseURL: "https://gviernes-grupo3-backend-fisi.herokuapp.com"
+    baseURL: "http://10.0.2.2:8000"
+    // baseURL: "https://gviernes-grupo3-backend-fisi.herokuapp.com"
 });
 
 export default instance;
